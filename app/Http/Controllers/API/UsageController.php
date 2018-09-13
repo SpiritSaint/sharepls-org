afad53243d66717885e3d768f4334ee2cd28593a
Ian Torres <iantorres@outlook.com>
2018-09-12 22:12:58 -0300

    Controller added

---
+++ app/Http/Controllers/API/UsageController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Usage;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UsageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Usage  $usage
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Usage $usage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Usage  $usage
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Usage $usage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Usage  $usage
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Usage $usage)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/API/VeredictController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Veredict;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class VeredictController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Veredict  $veredict
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Veredict $veredict)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Veredict  $veredict
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Veredict $veredict)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Veredict  $veredict
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Veredict $veredict)
+    {
+        //
+    }
+}
