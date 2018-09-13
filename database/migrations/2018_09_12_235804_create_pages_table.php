d264aff5f369b0affdc73267824cf3039d8ffee0
Ian Torres <iantorres@outlook.com>
2018-09-12 22:11:12 -0300

    Migration filled

--- database/migrations/2018_09_12_235804_create_pages_table.php
+++ database/migrations/2018_09_12_235804_create_pages_table.php
@@ -15,6 +15,14 @@ class CreatePagesTable extends Migration
     {
         Schema::create('pages', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('slug')->unique();
+            $table->string('keywords');
+            $table->string('title');
+            $table->string('subtitle');
+            $table->string('description');
+            $table->text('content');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
